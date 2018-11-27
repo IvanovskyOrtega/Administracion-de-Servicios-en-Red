8ff8b261982dd3aa928e59d641f20fa73b9f7d5f
Ivanovsky <ivanovskyortega@gmail.com>
2018-11-27 17:20:58 -0600

    COnfiguración de las interfaces de red

---
+++ loadbalancers/lb1/eth-interfaces.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+ifconfig eth0 10.0.2.1 netmask 255.255.255.0 up
+ifconfig eth1 10.0.2.2 netmask 255.255.255.0 up
+ifconfig eth2 up # Configurada por Keepalived
+ifconfig eth3 10.0.3.1 netmask 255.255.255.0 up
+ifconfig eth4 10.0.3.2 netmask 255.255.255.0 up
