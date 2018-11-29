6b7c02546a736ee16ad3da414f70c4a55699769a
Ivan Ortega Victoriano <iortegav1500@alumno.ipn.mx>
2018-11-29 17:55:39 -0600

    Corrección en la dirección de los servidores

--- loadbalancers/healthcheck/bothServers.sh
+++ loadbalancers/healthcheck/bothServers.sh
@@ -2,7 +2,7 @@
 iptables --flush
 iptables -t nat --flush
 iptables -t nat -p tcp -A POSTROUTING --source 10.0.2.0/24 -d 10.0.3.0/24 --dport 80 -j MASQUERADE
-iptables -t nat -p tcp -A PREROUTING -d 10.0.2.10 --dport 80 --match statistic --mode nth --every 2 --packet 0 -j DNAT --to-destination 10.0.3.1
-iptables -t nat -p tcp -A PREROUTING -d 10.0.2.10 --dport 80 -j DNAT --to-destination 10.0.3.2
-iptables -A FORWARD -p tcp -d 10.0.3.1 --dport 80 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
-iptables -A FORWARD -p tcp -d 10.0.3.2 --dport 80 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
+iptables -t nat -p tcp -A PREROUTING -d 10.0.2.10 --dport 80 --match statistic --mode nth --every 2 --packet 0 -j DNAT --to-destination 10.0.3.101
+iptables -t nat -p tcp -A PREROUTING -d 10.0.2.10 --dport 80 -j DNAT --to-destination 10.0.3.102
+iptables -A FORWARD -p tcp -d 10.0.3.101 --dport 80 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
+iptables -A FORWARD -p tcp -d 10.0.3.102 --dport 80 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
