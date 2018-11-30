e57ffbb376492f2ddc05bffb4470d4ecc1a573fa
Ivan Ortega Victoriano <iortegav1500@alumno.ipn.mx>
2018-11-29 19:26:32 -0600

    Cambio de ruta hacia los Load Balancer's

---
+++ routers/r4/checkroute.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+ip route add 10.0.2.0/24 via 10.0.2.13 dev eth3
+eth3=eth3
+eth4=eth4
+master_ip=10.0.2.13
+backup_ip=10.0.2.14
+master_interface=$eth3
+backup_interface=$eth4
+while :
+do
+	ping -c 1 -I $master_interface -W 1 10.0.2.10 > /dev/null 2>&1
+	ping_route=$?
+	if [ $ping_route -ne 0 ]; then
+		aux=$master_interface
+		master_interface=$backup_interface
+		backup_interface=$aux
+		aux=$master_ip
+		master_ip=$backup_ip
+		backup_ip=$aux
+		ip route flush dev $backup_interface
+		ip route add 10.0.2.0/24 via $master_ip dev $master_interface
+	fi
+	if [ $master_interface -ne $eth3 ]; then
+		ping -c 1 -I $master_interface -W 1 10.0.2.10 > /dev/null 2>&1
+		ping_route=$?
+		if [ $ping_route -eq 0 ]; then
+			master_interface=$eth3
+			backup_interface=$eth4
+			master_ip=10.0.2.13
+			backup_ip=10.0.2.14
+			ip route flush dev $backup_interface
+			ip route add 10.0.2.0/24 via $master_ip dev $master_interface
+		fi
+	fi
+	sleep 1
+done
