31f9f84d8e146574f503e28f396be24b3e5fcbcb
Ivan Ortega Victoriano <iortegav1500@alumno.ipn.mx>
2018-11-29 19:35:06 -0600

    Interfaces configuradas por Zebra

--- routers/r2/eth-interfaces.sh
+++ routers/r2/eth-interfaces.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-ifconfig eth0 192.168.1.1 netmask 255.255.255.0 up
-ifconfig eth1 10.0.1.13 netmask 255.255.255.0 up
-ifconfig eth2 10.0.1.14 netmask 255.255.255.0 up
-ip route add default via 192.168.1.254 dev eth0
+ifconfig eth0 up # Configurada por Zebra
+ifconfig eth1 up # Configurada por Zebra
+ifconfig eth2 up # Configurada por Zebra
