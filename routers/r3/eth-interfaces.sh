4b1463412aa864c45ab8f1e1d9c8d42fe530b753
Ivan Ortega Victoriano <iortegav1500@alumno.ipn.mx>
2018-11-29 18:26:51 -0600

    Solo se habilitan las interfaces
    Zebra y Keepalived configuran las IP de las interfaces

--- routers/r3/eth-interfaces.sh
+++ routers/r3/eth-interfaces.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-ifconfig eth0 10.0.1.1 netmask 255.255.255.0 up
-ifconfig eth1 10.0.1.2 netmask 255.255.255.0 up
+ifconfig eth0 up # Configurada por zebra
+ifconfig eth1 up # Configurada por zebra
 ifconfig eth2 up # Configurada por Keepalived
-ifconfig eth3 up # Configurada por Keepalived
-ifconfig eth4 10.0.2.11 netmask 255.255.255.0 up
-ifconfig eth5 10.0.2.12 netmask 255.255.255.0 up
+ifconfig eth3 up # Configurada por zebra
+ifconfig eth4 up # Configurada por zebra
