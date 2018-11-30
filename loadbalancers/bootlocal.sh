4885547bc226e464425a99109e67fe20004299eb
Ivan Ortega Victoriano <iortegav1500@alumno.ipn.mx>
2018-11-29 18:04:08 -0600

    Programas a ejecutar en el arranque

---
+++ loadbalancers/bootlocal.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+modprobe ipv6
+opt/eth-interfaces.sh &
+sysctl -w net.ipv4.ip_forward=1
+sysctl -w net.ipv6.conf.all.forwarding=1
+home/tc/healthcheck/healthcheck.sh &
+sudo keepalived -P -l -f /home/tc/conf/keepalived.conf
+sudo snmpd -C -c /home/tc/conf/snmpd.conf
