06a17f8bb2f5acb342868d10b2ce3f26ff7e79f2
Ivan Ortega Victoriano <iortegav1500@alumno.ipn.mx>
2018-11-29 18:34:08 -0600

    Cambio en las interfaces

--- routers/r4/eth-interfaces.sh
+++ routers/r4/eth-interfaces.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-ifconfig eth0 10.0.1.3 netmask 255.255.255.0 up
-ifconfig eth1 10.0.1.4 netmask 255.255.255.0 up
+ifconfig eth0 up # Configurada por Zebra
+ifconfig eth1 up # Configurada por Zebra
 ifconfig eth2 up # Configurada por Keepalived
-ifconfig eth3 up # Configurada por Keepalived
-ifconfig eth4 10.0.2.13 netmask 255.255.255.0 up
-ifconfig eth5 10.0.2.14 netmask 255.255.255.0 up
+ifconfig eth3 up # Configurada por Zebra
+ifconfig eth4 up # Configurada por Zebra
