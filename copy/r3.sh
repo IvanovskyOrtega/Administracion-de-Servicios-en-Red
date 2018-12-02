47819a56286fa4d1136b0af1a43faaebf0ae7201
Ivan Ortega Victoriano <iortegav1500@alumno.ipn.mx>
2018-12-02 10:30:36 -0600

    Update r3.sh

--- copy/r3.sh
+++ copy/r3.sh
@@ -3,7 +3,7 @@ sudo cp /mnt/sdb1/routers/r3/changeroute.sh ~
 sudo cp /mnt/sdb1/routers/r3/*.conf conf/
 sudo cp /mnt/sdb1/loadbalancers/snmpd.conf conf/
 sudo cp /mnt/sdb1/routers/r3/eth-interfaces.sh /opt/
+sudo cp /mnt/sdb1/routers/bootlocal.sh /opt/
 sudo chmod 777 *.sh
-sudo chmod 777 /opt/eth-interfaces.sh
+sudo chmod 777 /opt/*.sh
 sudo chmod 777 conf/*.conf
-sudo echo "home/tc/changeroute.sh &" >> /opt/bootlocal.sh
