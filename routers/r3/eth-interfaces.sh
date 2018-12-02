979398168cc380980d52343615910a40a702f815
Ivan Ortega Victoriano <iortegav1500@alumno.ipn.mx>
2018-12-02 09:53:32 -0600

    Update eth-interfaces.sh

--- routers/r3/eth-interfaces.sh
+++ routers/r3/eth-interfaces.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-ifconfig eth0 up # Configurada por zebra
-ifconfig eth1 up # Configurada por zebra
+ifconfig eth0 up # Configurada por Zebra
+ifconfig eth1 up # Configurada por Zebra
 ifconfig eth2 up # Configurada por Keepalived
-ifconfig eth3 up # Configurada por zebra
-ifconfig eth4 up # Configurada por zebra
+ifconfig eth3 up # Configurada por Keepalived
+ifconfig eth4 up # Configurada por Zebra
+ifconfig eth5 up # Configurada por Zebra
