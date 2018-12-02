dbd0931204b1d80824b918a50c87d937b1ac6675
Ivan Ortega Victoriano <iortegav1500@alumno.ipn.mx>
2018-12-02 09:39:16 -0600

    snmpd.conf

--- copy/r4.sh
+++ copy/r4.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 sudo cp /mnt/sdb1/routers/r4/changeroute.sh ~
 sudo cp /mnt/sdb1/routers/r4/*.conf conf/
+sudo cp /mnt/sdb1/loadbalancers/snmpd.conf conf/
 sudo cp /mnt/sdb1/routers/r4/eth-interfaces.sh /opt/
 sudo chmod 777 *.sh
 sudo chmod 777 /opt/eth-interfaces.sh
