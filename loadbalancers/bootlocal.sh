2949dd51127dfb83b1587384c758cc6e9970a552
Ivan Ortega Victoriano <iortegav1500@alumno.ipn.mx>
2018-12-02 10:20:26 -0600

    Update bootlocal.sh

--- loadbalancers/bootlocal.sh
+++ loadbalancers/bootlocal.sh
@@ -4,5 +4,6 @@ opt/eth-interfaces.sh &
 sysctl -w net.ipv4.ip_forward=1
 sysctl -w net.ipv6.conf.all.forwarding=1
 home/tc/healthcheck/healthcheck.sh &
+home/tc/changeroute.sh &
 sudo keepalived -P -l -f /home/tc/conf/keepalived.conf
 sudo snmpd -C -c /home/tc/conf/snmpd.conf
