317976f9a884a8e324227355e557e21058fce0fe
Ivan Ortega Victoriano <iortegav1500@alumno.ipn.mx>
2018-12-02 09:42:39 -0600

    snmpd.conf

--- copy/r2.sh
+++ copy/r2.sh
@@ -2,6 +2,7 @@
 sudo cp /mnt/sdb1/routers/r2/changeroute.sh ~
 sudo cp /mnt/sdb1/routers/r2/isplb.sh ~
 sudo cp /mnt/sdb1/routers/r2/*.conf conf/
+sudo cp /mnt/sdb1/loadbalancers/snmpd.conf conf/
 sudo cp /mnt/sdb1/routers/r2/eth-interfaces.sh /opt/
 sudo chmod 777 *.sh
 sudo chmod 777 /opt/eth-interfaces.sh
