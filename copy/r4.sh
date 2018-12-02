7a0fdf4cbc3f1725cc53cc27632ba484ae0fd063
Ivan Ortega Victoriano <iortegav1500@alumno.ipn.mx>
2018-12-02 10:31:17 -0600

    Update r4.sh

--- copy/r4.sh
+++ copy/r4.sh
@@ -3,7 +3,7 @@ sudo cp /mnt/sdb1/routers/r4/changeroute.sh ~
 sudo cp /mnt/sdb1/routers/r4/*.conf conf/
 sudo cp /mnt/sdb1/loadbalancers/snmpd.conf conf/
 sudo cp /mnt/sdb1/routers/r4/eth-interfaces.sh /opt/
+sudo cp /mnt/sdb1/routers/bootlocal.sh /opt/
 sudo chmod 777 *.sh
-sudo chmod 777 /opt/eth-interfaces.sh
+sudo chmod 777 /opt/*.sh
 sudo chmod 777 conf/*.conf
-sudo echo "home/tc/changeroute.sh &" >> /opt/bootlocal.sh
