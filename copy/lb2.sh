e2380a3c32321e40ff034fa55d1e4e59ea112ac6
Ivan Ortega Victoriano <iortegav1500@alumno.ipn.mx>
2018-12-02 09:38:07 -0600

    Se agregan archivos faltantes

--- copy/lb2.sh
+++ copy/lb2.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 sudo cp /mnt/sdb1/loadbalancers/healthcheck/*.sh healthcheck/
 sudo cp /mnt/sdb1/loadbalancers/lb2/*.conf conf/
+sudo cp /mnt/sdb1/loadbalancers/snmpd.conf conf/
+sudo cp /mnt/sdb1/loadbalancers/bootlocal.sh /opt/
 sudo cp /mnt/sdb1/loadbalancers/lb2/eth-interfaces.sh /opt/
 sudo chmod 777 healthcheck/*.sh
-sudo chmod 777 /opt/eth-interfaces.sh
+sudo chmod 777 /opt/*.sh
 sudo chmod 777 conf/*.conf
+
