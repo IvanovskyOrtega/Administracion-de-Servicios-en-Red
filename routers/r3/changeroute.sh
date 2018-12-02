7c448c637ce94675e569d1a4ad179fba7cfa89a0
Ivan Ortega Victoriano <iortegav1500@alumno.ipn.mx>
2018-12-02 09:53:11 -0600

    Update changeroute.sh

--- routers/r3/changeroute.sh
+++ routers/r3/changeroute.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
 
-ip route add 10.0.2.0/24 via 10.0.2.11 dev eth3
-eth3=eth3
+ip route flush dev eth4
+ip route flush dev eth5
+ip route add 10.0.2.0/24 proto kernel scope link src 10.0.2.11 dev eth4
 eth4=eth4
+eth5=eth5
 master_ip=10.0.2.11
 backup_ip=10.0.2.12
-master_interface=$eth3
-backup_interface=$eth4
+master_interface=$eth4
+backup_interface=$eth5
 while :
 do
 	ping -c 1 -I $master_interface -W 1 10.0.2.10 > /dev/null 2>&1
 	ping_route=$?
 	if [ $ping_route -ne 0 ]; then
+		echo "Backup interface working"
 		aux=$master_interface
 		master_interface=$backup_interface
 		backup_interface=$aux
@@ -19,18 +22,19 @@ do
 		master_ip=$backup_ip
 		backup_ip=$aux
 		ip route flush dev $backup_interface
-		ip route add 10.0.2.0/24 via $master_ip dev $master_interface
+		ip route add 10.0.2.0/24 proto kernel scope link src $master_ip dev $master_interface
 	fi
-	if [ $master_interface -ne $eth3 ]; then
-		ping -c 1 -I $master_interface -W 1 10.0.2.10 > /dev/null 2>&1
+	if [ $master_interface != $eth4 ]; then
+		ping -c 1 -I eth4 -W 1 10.0.2.10 > /dev/null 2>&1
 		ping_route=$?
 		if [ $ping_route -eq 0 ]; then
-			master_interface=$eth3
-			backup_interface=$eth4
+			echo "Master Interface recovered"
+			master_interface=$eth4
+			backup_interface=$eth5
 			master_ip=10.0.2.11
 			backup_ip=10.0.2.12
 			ip route flush dev $backup_interface
-			ip route add 10.0.2.0/24 via $master_ip dev $master_interface
+			ip route add 10.0.2.0/24 proto kernel scope link src $master_ip dev $master_interface
 		fi
 	fi
 	sleep 1
