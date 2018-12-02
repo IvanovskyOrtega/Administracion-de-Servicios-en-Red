69f446da9c263d274b5520b5760d79a60684e0d8
Ivan Ortega Victoriano <iortegav1500@alumno.ipn.mx>
2018-12-02 15:26:33 -0600

    Update bootlocal.sh

--- loadbalancers/bootlocal.sh
+++ loadbalancers/bootlocal.sh
@@ -4,6 +4,8 @@ opt/eth-interfaces.sh &
 sysctl -w net.ipv4.ip_forward=1
 sysctl -w net.ipv6.conf.all.forwarding=1
 home/tc/healthcheck/healthcheck.sh &
-home/tc/changeroute.sh &
+#home/tc/changeroute.sh &
+sudo zebra -u root -d -f /home/tc/conf/zebra.conf
+sudo ospfd -u root -d -f /home/tc/conf/ospfd.conf
 sudo keepalived -P -l -f /home/tc/conf/keepalived.conf
 sudo snmpd -C -c /home/tc/conf/snmpd.conf
