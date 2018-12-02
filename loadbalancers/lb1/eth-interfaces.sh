4b7a461aeadf2474dce5f92d38393b5bf079ae14
Ivan Ortega Victoriano <iortegav1500@alumno.ipn.mx>
2018-12-02 12:44:18 -0600

    Update eth-interfaces.sh

--- loadbalancers/lb1/eth-interfaces.sh
+++ loadbalancers/lb1/eth-interfaces.sh
@@ -4,3 +4,4 @@ ifconfig eth1 10.0.2.2 netmask 255.255.255.0 up
 ifconfig eth2 up # Configurada por Keepalived
 ifconfig eth3 10.0.3.1 netmask 255.255.255.0 up
 ifconfig eth4 10.0.3.2 netmask 255.255.255.0 up
+ifconfig eth5 up # Configurada por Keepalived
