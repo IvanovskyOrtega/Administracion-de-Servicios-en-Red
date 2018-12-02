8f194eccb7676a8f4e0fff2c478d328258dd531b
Ivan Ortega Victoriano <iortegav1500@alumno.ipn.mx>
2018-12-02 09:38:59 -0600

    Se agrega el snmpd.conf

--- copy/r3.sh
+++ copy/r3.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 sudo cp /mnt/sdb1/routers/r3/changeroute.sh ~
 sudo cp /mnt/sdb1/routers/r3/*.conf conf/
+sudo cp /mnt/sdb1/loadbalancers/snmpd.conf conf/
 sudo cp /mnt/sdb1/routers/r3/eth-interfaces.sh /opt/
 sudo chmod 777 *.sh
 sudo chmod 777 /opt/eth-interfaces.sh
