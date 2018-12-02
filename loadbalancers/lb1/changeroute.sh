a244d6144f921c55c2bec72dbe8e235d2728f293
Ivan Ortega Victoriano <iortegav1500@alumno.ipn.mx>
2018-12-02 10:38:04 -0600

    Update changeroute.sh

--- loadbalancers/lb1/changeroute.sh
+++ loadbalancers/lb1/changeroute.sh
@@ -3,7 +3,7 @@
 ip route flush dev eth0
 ip route flush dev eth1
 ip route add 10.0.2.0/24 proto kernel scope link src 10.0.2.1 dev eth0
-ip route add default 10.0.2.20
+route add default gw 10.0.2.20
 eth0=eth0
 eth1=eth1
 master_ip=10.0.2.1
@@ -24,7 +24,7 @@ do
 		backup_ip=$aux
 		ip route flush dev $backup_interface
 		ip route add 10.0.2.0/24 proto kernel scope link src $master_ip dev $master_interface
-		ip route add default 10.0.2.20
+		route add default gw 10.0.2.20
 	fi
 	if [ $master_interface != $eth0 ]; then
 		ping -c 1 -I eth0 -W 1 10.0.2.20 > /dev/null 2>&1
@@ -37,7 +37,7 @@ do
 			backup_ip=10.0.2.2
 			ip route flush dev $backup_interface
 			ip route add 10.0.2.0/24 proto kernel scope link src $master_ip dev $master_interface
-			ip route add default 10.0.2.20
+			route add default gw 10.0.2.20
 		fi
 	fi
 	sleep 1
