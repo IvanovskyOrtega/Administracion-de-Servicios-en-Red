9311d3769f4a7324a3be9a5b4cbe26e66faf5cbd
Ivan Ortega Victoriano <iortegav1500@alumno.ipn.mx>
2018-12-02 09:37:33 -0600

    De una vez el bootlocal.sh

--- copy/lb1.sh
+++ copy/lb1.sh
@@ -2,7 +2,9 @@
 sudo cp /mnt/sdb1/loadbalancers/healthcheck/*.sh healthcheck/
 sudo cp /mnt/sdb1/loadbalancers/lb1/*.conf conf/
 sudo cp /mnt/sdb1/loadbalancers/snmpd.conf conf/
+
+sudo cp /mnt/sdb1/loadbalancers/bootlocal.sh /opt/
 sudo cp /mnt/sdb1/loadbalancers/lb1/eth-interfaces.sh /opt/
 sudo chmod 777 healthcheck/*.sh
-sudo chmod 777 /opt/eth-interfaces.sh
+sudo chmod 777 /opt/*.sh
 sudo chmod 777 conf/*.conf
