04430089de0310b11b0c0e3dd87605d9108cc692
Ivan Ortega Victoriano <iortegav1500@alumno.ipn.mx>
2018-12-02 09:35:51 -0600

    Faltaba el archivo de snmpd

--- copy/lb1.sh
+++ copy/lb1.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 sudo cp /mnt/sdb1/loadbalancers/healthcheck/*.sh healthcheck/
 sudo cp /mnt/sdb1/loadbalancers/lb1/*.conf conf/
+sudo cp /mnt/sdb1/loadbalancers/snmpd.conf conf/
 sudo cp /mnt/sdb1/loadbalancers/lb1/eth-interfaces.sh /opt/
 sudo chmod 777 healthcheck/*.sh
 sudo chmod 777 /opt/eth-interfaces.sh
