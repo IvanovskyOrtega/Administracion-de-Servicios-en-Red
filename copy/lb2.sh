7fc2936e860ad500ee479fb9a49f2bfc98b1242d
Ivan Ortega Victoriano <iortegav1500@alumno.ipn.mx>
2018-12-02 10:22:07 -0600

    Update lb2.sh

--- copy/lb2.sh
+++ copy/lb2.sh
@@ -2,9 +2,11 @@
 sudo cp /mnt/sdb1/loadbalancers/healthcheck/*.sh healthcheck/
 sudo cp /mnt/sdb1/loadbalancers/lb2/*.conf conf/
 sudo cp /mnt/sdb1/loadbalancers/snmpd.conf conf/
+sudo cp /mnt/sdb1/loadbalancers/lb2/changeroute.sh ~
 sudo cp /mnt/sdb1/loadbalancers/bootlocal.sh /opt/
 sudo cp /mnt/sdb1/loadbalancers/lb2/eth-interfaces.sh /opt/
 sudo chmod 777 healthcheck/*.sh
 sudo chmod 777 /opt/*.sh
 sudo chmod 777 conf/*.conf
+sudo chmod 777 *.sh
 
