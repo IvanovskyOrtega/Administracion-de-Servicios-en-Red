728a83423132d09bd21da0452637c7379b98e89e
Ivan Ortega Victoriano <iortegav1500@alumno.ipn.mx>
2018-12-02 09:49:03 -0600

    Se agrega una interfaz

--- routers/r4/eth-interfaces.sh
+++ routers/r4/eth-interfaces.sh
@@ -2,5 +2,6 @@
 ifconfig eth0 up # Configurada por Zebra
 ifconfig eth1 up # Configurada por Zebra
 ifconfig eth2 up # Configurada por Keepalived
-ifconfig eth3 up # Configurada por Zebra
+ifconfig eth3 up # Configurada por Keepalived
 ifconfig eth4 up # Configurada por Zebra
+ifconfig eth5 up # Configurada por Zebra
