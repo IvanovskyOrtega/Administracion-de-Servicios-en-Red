b1fb3c3ae15185d2db79dc9fa796622a54181bb1
Ivan Ortega Victoriano <iortegav1500@alumno.ipn.mx>
2018-12-06 20:39:52 -0600

    Update eth-interfaces.sh

--- loadbalancers/lb1/eth-interfaces.sh
+++ loadbalancers/lb1/eth-interfaces.sh
@@ -3,5 +3,5 @@ ifconfig eth0 up # Configurada por Zebra
 ifconfig eth1 up # Configurada por Zebra
 ifconfig eth2 up # Configurada por Keepalived
 ifconfig eth3 up # Configurada por Keepalived
-ifconfig eth4 10.0.3.1 netmask 255.255.255.0 up
-ifconfig eth5 10.0.3.2 netmask 255.255.255.0 up
+ifconfig eth4 10.0.3.1 netmask 255.255.255.0 up # Interfaz primaria prara conectarse con los servidores
+ifconfig eth5 10.0.3.2 netmask 255.255.255.0 up # Interfaz secundaria prara conectarse con los servidores
