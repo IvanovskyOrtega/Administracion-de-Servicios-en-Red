f5abe707bf28fa20a6165cd2e0be918c7c4c3268
Ivan Ortega Victoriano <iortegav1500@alumno.ipn.mx>
2018-12-07 09:18:23 -0600

    Update eth-interfaces.sh

--- loadbalancers/lb2/eth-interfaces.sh
+++ loadbalancers/lb2/eth-interfaces.sh
@@ -3,5 +3,5 @@ ifconfig eth0 up # Configurada por Zebra
 ifconfig eth1 up # Configurada por Zebra
 ifconfig eth2 up # Configurada por Keepalived
 ifconfig eth3 up # Configurada por Keepalived
-ifconfig eth4 10.0.3.3 netmask 255.255.255.0 up
-ifconfig eth5 10.0.3.4 netmask 255.255.255.0 up
+ifconfig eth4 up # Configurada por Zebra
+ifconfig eth5 up # Configurada por Zebra
